948a0c4d3e288aa817eed8294727b93ae1d9abc2
長根尾貴仁 <taakaaneo@gmail.com>
2021-09-18 14:44:36 +0900

    feat: 列と行のValueObjectのバリデーションを削除

--- src/main/domain/value/seat/SeatColumn.ts
+++ src/main/domain/value/seat/SeatColumn.ts
@@ -3,7 +3,6 @@ import { PrimitiveValueObject } from "../PrimitiveValueObject";
 
 export class SeatColumn extends PrimitiveValueObject<string> {
   static create(value: string): SeatColumn {
-    if (!Number.isNaN(Number(value))) throw BusinessError.INVALID_COLUMN
     return new SeatColumn(value);
   }
 
--- src/main/domain/value/seat/SeatRow.ts
+++ src/main/domain/value/seat/SeatRow.ts
@@ -3,7 +3,6 @@ import { PrimitiveValueObject } from "../PrimitiveValueObject";
 
 export class SeatRow extends PrimitiveValueObject<string> {
   static create(value: string): SeatRow {
-    if (!Number.isNaN(Number(value))) throw BusinessError.INVALID_ROW
     return new SeatRow(value);
   }
 
