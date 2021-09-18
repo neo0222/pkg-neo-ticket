ae47185127037b62da6913b723bbc8cdd0a28949
長根尾貴仁 <taakaaneo@gmail.com>
2021-09-19 03:09:56 +0900

    feat: add 2nd floor in filter

--- src/main/domain/model/seat/Seat.ts
+++ src/main/domain/model/seat/Seat.ts
@@ -72,6 +72,7 @@ export class Seat extends EntityBase {
   }
 
   isAppropriate(): boolean {
-    return this.seatInfo.floor.value === '1' && this.seatInfo.row.value <= '10' && '15' <= this.seatInfo.column.value && this.seatInfo.column.value <= '28'
+    return (this.seatInfo.floor.value === '1' && this.seatInfo.row.value <= '10' && '15' <= this.seatInfo.column.value && this.seatInfo.column.value <= '28')
+     || (this.seatInfo.floor.value === '2' && this.seatInfo.row.value <= '1' && '15' <= this.seatInfo.column.value && this.seatInfo.column.value <= '28')
   }
 }
